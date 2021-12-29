a39dfb765599854ef54faf832aaa2594cc03c3e0
pranav-vijayananth <vijayananth.pranav@gmail.com>
2021-12-28 19:57:24 -0500

    Fixed up backend to some extent. Need to work on editing for other periods + DOM

--- script.js
+++ script.js
@@ -1,11 +1,124 @@
-// IMPORT FOR CALCULATING TIME PHRASES
+// all cases work for timeFrameCalculation  
 
-const { homeroomEditor } = require('./schedule');
-const schedule = require('./schedule')
+function timeFrameCalculation(durations) {
+    let startTime = 740; 
+    const passingTime = 5; 
+    let endTimeAdded; 
+    let timeFrames = [];
 
-homeroomEditor = schedule.homeroomEditor
+    for (let i = 0; i < durations.length; i++) {
+        
+        endTimeAdded = startTime + durations[i];
 
-// USELESS FUNC 
+        if (endTimeAdded % 100 >= 60 || durations[i] >= 60) {
+
+            let u = (endTimeAdded % 100); 
+            endTimeAdded -= (endTimeAdded % 100);
+            endTimeAdded += 100; 
+            endTimeAdded += (u - 60);
+
+        }
+
+        if ( ((endTimeAdded/100) | 0) > 12) {
+
+            endTimeAdded -= 1200;
+
+        } 
+
+        timeFrames.push(((startTime | 0) + " - " + (endTimeAdded | 0)));
+
+        startTime = endTimeAdded + passingTime;
+
+        if (startTime % 100 >= 60) {
+            
+            let h = (startTime % 100); 
+            startTime -= (startTime % 100);
+            startTime += 100; 
+            startTime += (h - 60);
+
+        }
+
+        if (((startTime/100) | 0) > 12) {
+            startTime -= 1200;
+        }
+
+    }
+
+    return timeFrames;
+
+}
+
+
+
+
+
+
+
+function homeroomEditor(newHomeroom) {
+
+
+    const defaultDurations = [5, 42, 42, 42, 13, 13, 13, 42, 42];
+    const defaultHomeroom = defaultDurations[0];
+
+    if (newHomeroom > defaultHomeroom) {
+       
+        let timeTaken = (newHomeroom - defaultHomeroom);
+        let timeCounter = 0;
+
+        while (timeCounter <= timeTaken) {
+        for (let i = 1; i < defaultDurations.length; i++) {
+
+            timeCounter++;
+
+            if (timeCounter > timeTaken) {
+            break;
+            }
+
+            defaultDurations[i]--;
+
+        }
+
+        }
+
+    }
+
+    else if (defaultHomeroom > newHomeroom) {
+        
+        let timeGiven = (defaultHomeroom - newHomeroom);
+        let timeCounter2 = 0;
+
+        while  (timeCounter <= timeGiven) {
+
+        for (let i = 0; i < defaultDurations.length; i++) {
+            timeCounter2++;
+
+            if (timeCounter2 > timeGiven) {
+            break;
+            }
+
+            defaultDurations[i]++;
+
+        }
+
+        }
+
+    }
+
+    else {}
+
+    defaultDurations[0] = newHomeroom;
+
+    return defaultDurations;
+
+}
+
+
+
+
+
+
+
+// useless ass functions
 
 function removeLunches(item) {
     if (item != 24) {
@@ -13,7 +126,28 @@ function removeLunches(item) {
     }
 }
 
-// DOM CODE 
+function insertLunchPeriods(durations) {
+    
+    const lunchDurations = 24; 
+    const lunchPeriods = [4, 6, 8, 10];
+    
+    // didn't use a for loop because i get a TypeError and im lazy as fuck to change it
+    durations.splice(lunchPeriods[0], 0, lunchDurations);
+    durations.splice(lunchPeriods[1], 0, lunchDurations);
+    durations.splice(lunchPeriods[2], 0, lunchDurations);
+    durations.splice(lunchPeriods[3], 0, lunchDurations);
+
+    return durations;
+    
+}
+
+
+
+
+
+
+
+// *** function linked for onclick() *** //
 
 function calculate() {
     var durationInputs = [];
@@ -24,10 +158,26 @@ function calculate() {
         durationInputs.push(strippedDuration);
     }
 
-    durationInputs = durationInputs.filter(removeLunches)
+    // filtered out lunches for homeroomEditor method
+    durationInputs = durationInputs.filter(removeLunches); 
 
-    
+    // changed all period durations based on updated homeroom duration
+    let updatedDurations = homeroomEditor(durationInputs[0]);
+
+    // inserting lunch periods for frame calculation method
+    updatedDurations = insertLunchPeriods(updatedDurations);
+
+    // converting durations into timeframes
+    let updatedTimeFrames = timeFrameCalculation(updatedDurations);
+
+    const trList = document.querySelectorAll("tr");
+
+    for (let i = 0; i < trList.length; i++) {
+        console.log(trList[i + 1].children[1])
+        console.log(trList[i + 1].children[2])
+    }
+
+
 
 
 }
-
